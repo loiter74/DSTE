7ae91ba33ba3abeaa8315e299e1a18af3962e406
loiter74 <302626787@qq.com>
2023-09-05 14:43:24 +0800

    gat complete

--- Spatial_Layer.py
+++ Spatial_Layer.py
@@ -6,123 +6,88 @@ Created on Mon Sep  4 16:29:48 2023
 """
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-
-class SpatialAttention(nn.Module):
-    # dartboard project + MSA
-    def __init__(self,
-                 dim,
-                 heads=4,
-                 qkv_bias=False,
-                 qk_scale=None,
-                 dropout=0.,
-                 num_sectors=17,
-                 assignment=None,
-                 mask=None):
-        super().__init__()
-        assert dim % heads == 0, f"dim {dim} should be divided by num_heads {heads}."
-
-        self.dim = dim
-        self.num_heads = heads
-        head_dim = dim // heads
-        self.scale = qk_scale or head_dim ** -0.5
-        self.num_sector = num_sectors
-        self.assignment = assignment  # [n, n, num_sector]
-        self.mask = mask  # [n, num_sector]
-
-        self.q_linear = nn.Linear(dim, dim, bias=qkv_bias)
-        self.kv_linear = nn.Linear(dim, dim * 2, bias=qkv_bias)
-
-        self.relative_bias = nn.Parameter(torch.randn(heads, 1, num_sectors))
-        self.proj = nn.Linear(dim, dim)
-        self.proj_drop = nn.Dropout(dropout)
-
-    def forward(self, x):
-        # x: [b, n, c]
-
-        B, N, C = x.shape
-
-        # query: [bn, 1, c]
-        # key/value target: [bn, num_sector, c]
-        # [b, n, num_sector, c]
-        pre_kv = torch.einsum('bnc,mnr->bmrc', x, self.assignment)
-
-        pre_kv = pre_kv.reshape(-1, self.num_sector, C)  # [bn, num_sector, c]
-        pre_q = x.reshape(-1, 1, C)  # [bn, 1, c]
-
-        q = self.q_linear(pre_q).reshape(B*N, -1, self.num_heads, C //
-                                         self.num_heads).permute(0, 2, 1, 3)  # [bn, num_heads, 1, c//num_heads]
-        kv = self.kv_linear(pre_kv).reshape(B*N, -1, 2, self.num_heads,
-                                            C // self.num_heads).permute(2, 0, 3, 1, 4)
-        k, v = kv[0], kv[1]  # [bn, num_heads, num_sector, c//num_heads]
-        attn = (q @ k.transpose(-2, -1)) * self.scale
-        attn = attn.reshape(B, N, self.num_heads, 1,
-                            self.num_sector) + self.relative_bias # you can fuse external factors here as well
-        mask = self.mask.reshape(1, N, 1, 1, self.num_sector)
-
-        # masking
-        attn = attn.masked_fill_(mask, float(
-            "-inf")).reshape(B * N, self.num_heads, 1, self.num_sector).softmax(dim=-1)
-
-        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
-        x = self.proj(x)
-        x = self.proj_drop(x)
-        return x
-
-
-class DS_MSA(nn.Module):
-    # Dartboard Spatial MSA
-    def __init__(self,
-                 dim,  # hidden dimension
-                 depth,  # number of MSA in DS-MSA
-                 heads,  # number of heads
-                 mlp_dim,  # mlp dimension
-                 assignment,  # dartboard assignment matrix
-                 mask,  # mask
-                 dropout=0.):  # dropout rate
-        super().__init__()
-        self.layers = nn.ModuleList([])
-        for i in range(depth):
-            self.layers.append(nn.ModuleList([
-                SpatialAttention(dim, heads=heads, dropout=dropout,
-                                 assignment=assignment, mask=mask,
-                                 num_sectors=assignment.shape[-1]),
-                PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout))
-            ]))
-
-    def forward(self, x):
-        # x: [b, c, n, t]
-        b, c, n, t = x.shape
-        x = x.permute(0, 3, 2, 1).reshape(b*t, n, c)  # [b*t, n, c]
-        # x = x + self.pos_embedding  # [b*t, n, c]  we use relative PE instead
-        for attn, ff in self.layers:
-            x = attn(x) + x
-            x = ff(x) + x
-        x = x.reshape(b, t, n, c).permute(0, 3, 2, 1)
-        return x
+class GAT(nn.Module):
+    def __init__(self, n_feat, n_hid, n_class, dropout, alpha, n_heads):
+        """Dense version of GAT
+        """
+        super(GAT, self).__init__()
+        self.dropout = dropout 
+        # 定义multi-head的图注意力层
+        self.attentions = [GraphAttentionLayer(n_feat, n_hid, dropout=dropout, alpha=alpha, concat=True) for _ in range(n_heads)]
+        for i, attention in enumerate(self.attentions):
+            self.add_module('attention_{}'.format(i), attention)
+        # n_heads 表示有几个GAL层，最后进行拼接在一起，类似self-attention从不同的子空间进行抽取特征。
+        self.out_att = GraphAttentionLayer(n_hid * n_heads, n_class, dropout=dropout,alpha=alpha, concat=False)
     
-# Pre Normalization in Transformer
-class PreNorm(nn.Module):
-    def __init__(self, dim, fn):
-        super().__init__()
-        self.norm = nn.LayerNorm(dim)
-        self.fn = fn
+    def forward(self, x, adj):
+        # x: [b, n, t, c]
+        b, n, t, c = x.shape
+        x = x.permute(0, 2, 1, 3).reshape(b*t, n, c)  # [b*t, n, c]
+        x = F.dropout(x, self.dropout, training=self.training)  
+        x = torch.cat([att(x, adj) for att in self.attentions], dim=2)  # 将每个head得到的表示进行拼接
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.elu(self.out_att(x, adj))   # 输出并激活
+        x = x.reshape(b, n, t, c) #恢复形状
+        return x
 
-    def forward(self, x, **kwargs):
-        return self.fn(self.norm(x), **kwargs)
+class nconv(nn.Module):
+    def __init__(self):
+        super(nconv, self).__init__()
 
-# FFN in Transformer
-class FeedForward(nn.Module):
-    def __init__(self, dim, hidden_dim, dropout=0.):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(dim, hidden_dim),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Linear(hidden_dim, dim),
-            nn.Dropout(dropout)
-        )
-    def forward(self, x):
-        return self.net(x)
+    def forward(self, x, A):
+        x = torch.einsum('ncvl,vw->ncwl', (x, A))
+        return x.contiguous()
+
+class GraphAttentionLayer(nn.Module):
+    """
+    图注意力层
+    """
+    def __init__(self, in_features, out_features, dropout, alpha, concat=True):
+        super(GraphAttentionLayer, self).__init__()
+        self.in_features = in_features   # 节点表示向量的输入特征数
+        self.out_features = out_features   # 节点表示向量的输出特征数
+        self.dropout = dropout    # dropout参数
+        self.alpha = alpha     # leakyrelu激活的参数
+        self.concat = concat   # 如果为true, 再进行elu激活
+        
+        # 定义可训练参数，即论文中的W和a
+        self.W = nn.Parameter(torch.zeros(size=(in_features, out_features)))  
+        nn.init.xavier_uniform_(self.W.data, gain=1.414)  # 初始化
+        self.a = nn.Parameter(torch.zeros(size=(2*out_features, 1)))
+        nn.init.xavier_uniform_(self.a.data, gain=1.414)   # 初始化
+        
+        # 定义leakyrelu激活函数
+        self.leakyrelu = nn.LeakyReLU(self.alpha)
+    
+    def forward(self, inp, adj):
+        """
+        inp: input_fea [B, N, in_features]  in_features表示节点的输入特征向量元素个数
+        adj: 图的邻接矩阵  [N, N] 非零即一，数据结构基本知识
+        """
+        h = torch.matmul(inp, self.W)   # [B, N, out_features] 384 10 16
+        N = h.size()[1]    # N 图的节点数
+        a_input = torch.cat([h.repeat(1,1,N).view(-1, N*N, self.out_features), h.repeat(1, N, 1)], dim=-1)
+        a_input = a_input.view(-1, N, N, 2*self.out_features)
+        # [B, N, N, 2*out_features]
+        e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(3))
+
+        # [B, N, N, 1] => [B, N, N] 图注意力的相关系数（未归一化）        
+        zero_vec = -1e12 * torch.ones_like(e)    # 将没有连接的边置为负无穷
+
+        attention = torch.where(adj>0, e, zero_vec)   # [B, N, N]
+        # 表示如果邻接矩阵元素大于0时，则两个节点有连接，该位置的注意力系数保留，
+        # 否则需要mask并置为非常小的值，原因是softmax的时候这个最小值会不考虑。
+        attention = F.softmax(attention, dim=1)    # softmax形状保持不变 [B, N, N]，得到归一化的注意力权重！
+        attention = F.dropout(attention, self.dropout, training=self.training)   # dropout，防止过拟合
+        h_prime = torch.matmul(attention, h)  # [B, N, N].[B, N, out_features] => [B, N, out_features]
+        # 得到由周围节点通过注意力权重进行更新的表示
+        if self.concat:
+            return F.relu(h_prime)
+        else:
+            return h_prime 
+    
+ 
     
     
--- test.py
+++ test.py
@@ -1,40 +1,27 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
+
 from torch.utils.data import DataLoader
-from Spatial_Layer import DS_MSA
+from Spatial_Layer import GAT
 
 # 定义超参数
-hidden_dim = 512
-num_layers = 6
-num_heads = 8
-mlp_dim = 1024
-dropout_rate = 0.1
-batch_size = 32
+batch_size = 16
 learning_rate = 1e-4
 num_epochs = 10
 
-# 创建虚拟数据
-# 生成具有您指定维度的随机数据
-# 请根据您的具体任务和数据格式进行修改
-num_samples = 1000
-data_shape = (128, 100, 24, 32)
+num_samples = 100
+data_shape = (10, 24, 16)
 dummy_data = torch.randn(num_samples, *data_shape)
+adj = torch.tensor(np.random.rand(10, 10))
 
 # 创建数据加载器
 train_dataset = torch.utils.data.TensorDataset(dummy_data)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
 # 创建模型
-model = DS_MSA(
-    dim=hidden_dim,
-    depth=num_layers,
-    heads=num_heads,
-    mlp_dim=mlp_dim,
-    assignment=None,  # 替换为您的分配矩阵
-    mask=None,  # 替换为您的掩码
-    dropout=dropout_rate
-)
+model = GAT(n_feat=16, n_hid=32, n_class=16, dropout=0.2, alpha=0.01, n_heads=4)
 
 # 定义损失函数和优化器
 criterion = nn.MSELoss()  # 使用均方误差作为示例损失函数
@@ -48,7 +35,7 @@ for epoch in range(num_epochs):
     for batch_data in train_loader:
         inputs = batch_data[0]  # 获取输入数据
         optimizer.zero_grad()
-        outputs = model(inputs)
+        outputs = model(inputs, adj)
         loss = criterion(outputs, inputs)  # 使用均方误差计算损失
         loss.backward()
         optimizer.step()
